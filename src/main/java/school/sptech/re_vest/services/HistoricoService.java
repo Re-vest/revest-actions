fc94915fa4a89c3cb1ee6247f13b039397f7b495
antunesGustavo <Gustavooliver.antunes@gmail.com>
2025-06-05 17:38:05 -0300

    Ajustes finalizado

--- src/main/java/school/sptech/re_vest/services/HistoricoService.java
+++ src/main/java/school/sptech/re_vest/services/HistoricoService.java
@@ -2,11 +2,13 @@ package school.sptech.re_vest.services;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import school.sptech.re_vest.domain.Historico;
 import school.sptech.re_vest.repositories.HistoricoRepository;
 import school.sptech.re_vest.utils.FilaObj;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -22,15 +24,24 @@ public class HistoricoService {
         this.filaHistorico = new FilaObj<>(100);
     }
 
-    public void registrarHistorico(Integer idUsuario, String acao){
+    public void registrarHistorico(Integer idUsuario, String nomeUsuario, String acao){
         Historico historico = new Historico();
         historico.setIdUsuario(idUsuario);
+        historico.setNomeUsuario(nomeUsuario);
         historico.setAcao(acao);
         historico.setDataHora(LocalDateTime.now());
 
         filaHistorico.insert(historico);
         historicoRepository.save(historico);
     }
+
+    @Scheduled(cron = "0 0 0 1 1,3,5,7,9,11 ?")
+    public void limparHistoricoAntigo() {
+        LocalDate limite = LocalDate.now().minusMonths(2);
+        historicoRepository.deletarPorDataAntesDe(limite);
+        System.out.println("Histórico anterior a " + limite + " foi limpo.");
+    }
+
 
     public List<Historico> listarTodos() {
         return historicoRepository.listarTodos();
